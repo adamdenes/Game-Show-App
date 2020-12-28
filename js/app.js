81de940eaf4e48c19fdd31b29631bb96836e24e8
Adam Denes <adam.denes@icloud.com>
2020-12-28 12:02:48 +0100

    create base functions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('working');
+
+    // functions
+
+    /**
+     * Randomly choose a phrase from the phrases array and split that phrase into a new array of characters.
+     * @param {Object[]} arr 
+     */
+    const getRandomPhraseAsArray = arr => arr[Math.floor(Math.random() * arr.length)].split('');
+
+    /**
+     * For each character in the array create a list item and append to `#phrase ul` element.
+     * @param {Object[]} arr 
+     */
+    const addPhraseToDisplay = arr => {
+        arr.forEach(element => {
+            const ul = phraseDiv.firstElementChild;
+            const li = document.createElement('li');
+            li.textContent = element;
+            ul.appendChild(li);
+
+            if (element !== ' ') {
+                li.setAttribute('class', 'letter');
+            }
+        });
+    };
+
+    // element selectors & variables
+    const qwertyDiv = document.querySelector('#qwerty');
+    const phraseDiv = document.querySelector('#phrase');
+    const resetBtn = document.querySelector('.btn__reset');
+    let missed = 0;
+
+    // phrases array
+    const phrases = new Array();
+    phrases.push(
+        'What Goes Up Must Come Down',
+        'Hard Pill to Swallow',
+        'Right Off the Bat',
+        'Down And Out',
+        'Man of Few Words'
+    );
+
+    const phraseArray = getRandomPhraseAsArray(phrases);
+    console.log(phraseArray);
+    console.log(addPhraseToDisplay(phraseArray));
 });
